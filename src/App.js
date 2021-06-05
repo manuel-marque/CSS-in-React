ba232e64b45a067aab903a1af3c12c23dc569368
Manuel Marque <manuelmarq05@gmail.com>
2021-06-05 16:38:43 +0200

    challenge CSS in React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+  padding: 10px;
+  color: #444;
+  border: 1px solid #cdcdcd;
+  width: 220px;
+  height: 220px;
+  border-radius: 10px;
+  margin: 1em;
+
+`;
+
+const Image = styled.img`
+  margin-right: 1em;
+  width: 48px;
+  height: 48px;
+  background-size: 48px 48px;
+  background-image: url("https://avatars.githubusercontent.com/u/75366576");
+`;
+const SubContainer = styled.div`
+  width: 8px;
+  height: 8px;
+  margin-right: 6px;
+`;
+
+const Title = styled.h5`
+  margin: 0 0 0.25rem;
+  color: #252850;
+  padding: 10px;
+  width: 163px;
+
+`;
+const P = styled.p`
+  margin: 7px 21px 0.25rem;
+  color: #252850;
+  font-size: 12px;
+  padding: 10px;
+  width: 155px;
+
+`;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Image />
+        <SubContainer>
+          <Title>Valores predeterminado en formulario</Title>
+          <P>Formulario de campos con valores predeterminado</P>
+        </SubContainer>
+      </Container>
     </div>
   );
 }
